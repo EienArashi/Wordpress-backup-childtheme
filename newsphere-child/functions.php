11a3360bfb57e1ab2853bfb1e044236b86aec73e
EienArashi <arashi_usu@hotmail.com>
2019-09-27 10:34:03 +0200

    added function

--- newsphere-child/functions.php
+++ newsphere-child/functions.php
@@ -8,6 +8,15 @@ function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
 
+//add a kind greeting with elseif code
+$t = date("H");
+
+if ($t < "20") {
+    echo "Have a good day!";
+} else {
+    echo "Have a good night!";
+}
+
 //adding custom logo to override WordPress logo.
 function tr_dashboard_logo() {
 	echo '<style type="text/css"> #wpadminbar #wp-admin-bar-wp-logo > .ab-item .ab-icon:before{color:rgba(0, 0, 0, 0); background-position: 0 0; background-image: url('.get_bloginfo('stylesheet_directory').'/images/dashboard-logo.png) !important;} #wpadminbar #wp-admin-bar-wp-logo.hover > .ab-item .ab-icon{background-position: 0 0;}</style>';
